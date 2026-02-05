398049f112075114f7d601d328a65b5ebea83c20
Muhammad Hamza Mahmood <mhamzamahmood5@gmail.com>
2026-02-05 16:27:26 +0500

    Interface in TypeScript

--- tutorial.ts
+++ tutorial.ts
@@ -22,3 +22,35 @@ function add(a: number, b: number): number {
 const concatStrings = (str1: string, str2: string): string => {
   return str1 + str2;
 };
+
+// Interface in TypeScript
+interface Person {
+  id: number;
+  name: string;
+  age?: number; // Use ? to make property optional
+  isEmployed: boolean;
+  greet(message: string): void; // void means it does not return anything
+}
+
+// Object adhering to the Person interface
+const Person1: Person = {
+  id: 1,
+  name: "Alice",
+  age: 30,
+  isEmployed: true,
+  greet: (message: string) => {
+    console.log(`Hello ${message}`);
+  },
+};
+
+const Person2: Person = {
+  id: 2,
+  name: "Bob",
+  isEmployed: false,
+  greet: (message: string) => {
+    console.log(`Hi ${message}`);
+  },
+};
+
+Person1.greet(`Welcome to TypeScript! ${Person1.name}`);
+Person2.greet(`Welcome to TypeScript! ${Person2.name}`);
